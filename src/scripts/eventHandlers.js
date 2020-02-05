25499724ec56eff27ed37352ce955ca3f3ed9dac
Keith Potempa <keithrpotempa@gmail.com>
2020-02-05 15:02:06 -0600

    phase one complete, outputting with repeat to the dom

--- src/scripts/eventHandlers.js
+++ src/scripts/eventHandlers.js
@@ -1,11 +1,13 @@
 import domFactory from "./domFactory.js";
+import reps from "./main.js";
 
 const eventHandlers = {
     addSendListener() {
         const sendBtn = document.querySelector("#send__button")
         sendBtn.addEventListener("click", (e) => {
             let plea = document.querySelector("#plea").value;
-            domFactory.renderer.plea(plea)
+            let times = document.querySelector("#repetitions").value.split("__")[1];
+            domFactory.renderer.plea(plea, times)
             document.querySelector("#plea").value = ""
         })
     }
